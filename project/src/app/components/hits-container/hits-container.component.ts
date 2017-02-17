a2acd418e8d31523a16070e06300572e2c8c1305
Gabriel Costa <gabrielp.costa@outlook.com>
2017-02-17 11:50:13 -0400

    Modificações no hit container para exibir o total de cliques baseado no json

--- project/src/app/components/hits-container/hits-container.component.ts
+++ project/src/app/components/hits-container/hits-container.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { UrlItem } from '../shared/url-item';
+import { UrlItemDataService } from '../shared/url-item-data.service';
 
 @Component({
   selector: 'app-hits-container',
@@ -6,7 +9,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./hits-container.component.scss']
 })
 
-export class HitsContainerComponent {
-  constructor() {}
+export class HitsContainerComponent implements OnInit {
+  urlItems: UrlItem[] = [];
+  totalClicks: number;
+
+  constructor(private _urlItemDataService:UrlItemDataService) {
+    this._urlItemDataService.getAllUrlItems().subscribe(this.onDataUpdate.bind(this));
+  }
+
+  ngOnInit() { 
+    if(this.urlItems.length == 0)
+     this._urlItemDataService.getAllUrlItems().subscribe(this.onDataUpdate.bind(this)); 
+  }
+
+  onDataUpdate(data: any) {
+      this.urlItems = data; 
+      this.totalClicks = this.getTotalClicks();
+  }
+
+  getTotalClicks(): number {
+    var count: number = 0;
+    for(var i = 0; i < this.urlItems.length; ++i) {
+        count += this.urlItems[i].hits;
+    }
+
+    return count;
+  }
 }
 
