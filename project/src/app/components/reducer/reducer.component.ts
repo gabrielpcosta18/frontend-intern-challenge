cabdf10ae4e094f76fb738e2958e7c1da3a432fb
Gabriel Costa <gabrielp.costa@outlook.com>
2017-02-17 11:51:38 -0400

    Modificações no input do Reducer Container seguindo comportamento do GIF
    Não finalizado. Necessário aplicar o algoritmo de encurtamento de URL e funcionalidade de cópia do texto para o clipboard.

--- project/src/app/components/reducer/reducer.component.ts
+++ project/src/app/components/reducer/reducer.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input, ChangeDetectorRef } from '@angular/core';
 
 @Component({
   selector: 'app-reducer',
@@ -7,6 +7,33 @@ import { Component } from '@angular/core';
 })
 
 export class ReducerComponent {
-   constructor() { }
+  showRemoveButton: boolean = false;
+  reductUrlButtonText: string = "Encurtar";
+  inputTextValue: string = '';
+  
+  constructor(private ref: ChangeDetectorRef) { 
+    ref.detach();
+    setInterval(() => {
+      this.ref.detectChanges();
+    }, 500);
+
+  }
+
+  reductUrlButtonClick(element) {
+    console.log(this.showRemoveButton);
+    this.showRemoveButton = !this.showRemoveButton;
+
+    if(this.showRemoveButton) 
+      this.reductUrlButtonText = "Copiar";
+    else this.reductUrlButtonText = "Encurtar";
+
+    //element.textContent = this.reductUrlButtonText;
+  }
+
+  removeButtonClick() {
+    this.showRemoveButton = false;
+    this.inputTextValue = '';
+    this.reductUrlButtonText = "Encurtar";
+  }
 }
 
